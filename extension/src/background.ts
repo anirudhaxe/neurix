c993467294dc6b53a88ba32a65b8ab67e7378760
Aniruddha Chaudhary <axedotdev@outlook.com>
2025-11-01 15:40:53 +0530

    chore: fixed background service worker

---
+++ extension/src/background.ts
@@ -0,0 +1,5 @@
+// Background service worker for the extension
+console.log('[CRXJS] Background service worker loaded')
+
+// This is a minimal background service worker
+// You can add background functionality here as needed
