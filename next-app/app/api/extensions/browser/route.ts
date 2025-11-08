04a862dc532384185307e7acc9d5f091dcd1c10f
Aniruddha Chaudhary <axedotdev@outlook.com>
2025-11-08 13:22:17 +0530

    chore: added api route

---
+++ next-app/app/api/extensions/browser/route.ts
@@ -0,0 +1,25 @@
+export async function POST(request: Request) {
+  const data = await request.json();
+
+  console.log(data.text);
+
+  const response = Response.json({
+    received: "ok",
+  });
+
+  // Set CORS headers (origin, methods, headers)
+  response.headers.set("Access-Control-Allow-Origin", "*");
+  response.headers.set("Access-Control-Allow-Methods", "POST, OPTIONS");
+  response.headers.set("Access-Control-Allow-Headers", "Content-Type");
+
+  return response;
+}
+
+// OPTIONS method for preflight requests
+export async function OPTIONS() {
+  const response = new Response(null, { status: 200 });
+  response.headers.set("Access-Control-Allow-Origin", "*");
+  response.headers.set("Access-Control-Allow-Methods", "POST, OPTIONS");
+  response.headers.set("Access-Control-Allow-Headers", "Content-Type");
+  return response;
+}
