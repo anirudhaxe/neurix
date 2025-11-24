c0e672230eec4396cdc5c155bd42b405aec5ed88
Aniruddha Chaudhary <axedotdev@outlook.com>
2025-11-24 23:30:09 +0530

    chore: added vector code

--- hono-worker/queue-workers/index.ts
+++ hono-worker/queue-workers/index.ts
@@ -1,10 +1,52 @@
 import { Worker } from "bullmq";
 import kvConnection from "../kv";
+import { OpenAIEmbeddings } from "@langchain/openai";
+import { Document } from "langchain";
+import { QdrantVectorStore } from "@langchain/qdrant";
+import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
 
 const jobQueueWorker = new Worker(
   "jobQueue",
   async (job) => {
-    console.log(job.data);
+    try {
+      // This splitter will recursively split the document using common separators like new lines until each chunk is of appropriate size.
+      const textSplitter = new RecursiveCharacterTextSplitter({
+        chunkSize: 500, // split in chunks of 500 characters
+        chunkOverlap: 100, // 100 characters overlap between the chunks. The overlap helps mitigate the possibility of separating a statement from important context related to it.
+      });
+
+      const splittedtext = await textSplitter.splitText(job.data.textData);
+
+      // create langchain documents from splited text
+      const documents = splittedtext.map(
+        (text) => new Document({ pageContent: text }),
+      );
+
+      console.log("CREATED DOCUMENTS LENGTH: ", documents.length);
+
+      // initialize embeddings model instance
+      const embeddings = new OpenAIEmbeddings({
+        model: "openai/text-embedding-3-small",
+        apiKey: process.env.EMBEDDING_MODEL_KEY,
+      });
+
+      // initialize the vector store and add documents
+      const vectorStore = await QdrantVectorStore.fromExistingCollection(
+        embeddings,
+        {
+          url: "http://localhost:6333",
+          collectionName: "neurix-vector-testing",
+        },
+      );
+
+      await vectorStore.addDocuments(documents);
+
+      console.info(
+        "INFO: DOCUMENT ADDED TO VECTOR STORE, SEMANTIC SEARCH ENGINE READY",
+      );
+    } catch (error) {
+      console.error("ERROR: WHILE STORING VECTORS: ", error);
+    }
   },
   {
     connection: kvConnection,
