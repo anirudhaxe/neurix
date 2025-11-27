984ff52bfd9a2b3af3d229a62314f3d6bd311722
Aniruddha Chaudhary <axedotdev@outlook.com>
2025-11-27 17:34:33 +0530

    chore: added chat and message tables

--- next-app/db/schema/base-schema.ts
+++ next-app/db/schema/base-schema.ts
@@ -1,8 +1,59 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import {
+  integer,
+  pgTable,
+  // boolean,
+  // foreignKey,
+  json,
+  // jsonb,
+  // primaryKey,
+  text,
+  timestamp,
+  uuid,
+  varchar,
+} from "drizzle-orm/pg-core";
+import type { InferSelectModel } from "drizzle-orm";
+import { user } from "./auth-schema";
 
+// sample table, will be removed later
 export const playersTable = pgTable("players", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   name: varchar({ length: 255 }).notNull(),
   age: integer().notNull(),
   email: varchar({ length: 255 }).notNull().unique(),
 });
+
+// chat table
+export const chat = pgTable("chat", {
+  id: uuid("id").primaryKey().notNull().defaultRandom(),
+  title: text("title").notNull(),
+  userId: text("user_id").references(() => user.id),
+  visibility: varchar("visibility", { enum: ["public", "private"] })
+    .notNull()
+    .default("private"),
+  // optionally implement tokenlens later
+  // lastContext: jsonb("lastContext").$type<AppUsage | null>(),
+  updatedAt: timestamp("updated_at")
+    .defaultNow()
+    .$onUpdate(() => new Date())
+    .notNull(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+});
+
+export type Chat = InferSelectModel<typeof chat>;
+
+export const message = pgTable("message", {
+  id: uuid("id").primaryKey().notNull().defaultRandom(),
+  chatId: uuid("chat_id")
+    .notNull()
+    .references(() => chat.id),
+  role: varchar("role").notNull(),
+  parts: json("parts").notNull(),
+  attachments: json("attachments").notNull(),
+  updatedAt: timestamp("updated_at")
+    .defaultNow()
+    .$onUpdate(() => new Date())
+    .notNull(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+});
+
+export type DBMessage = InferSelectModel<typeof message>;
