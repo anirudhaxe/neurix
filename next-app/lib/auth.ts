91126164b9b3301dd711075793bc6325c1bd0368
Aniruddha Chaudhary <axedotdev@outlook.com>
2025-12-09 15:26:51 +0530

    refactor: refactored auth code

--- next-app/lib/auth.ts
+++ next-app/lib/auth.ts
@@ -3,9 +3,11 @@ import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 
 const auth = betterAuth({
+  // database
   database: drizzleAdapter(db, {
     provider: "pg",
   }),
+  // authentication methods
   emailAndPassword: {
     enabled: true,
   },
