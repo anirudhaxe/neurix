c8f3b3dc01b1f1ea05a60568bf509f590e043fdc
Aniruddha Chaudhary <axedotdev@outlook.com>
2025-12-10 14:43:28 +0530

    fix: fixed proxy setup and signout function

--- next-app/lib/auth/auth-client.ts
+++ next-app/lib/auth/auth-client.ts
@@ -1,13 +1,15 @@
 import { createAuthClient } from "better-auth/react";
-import { redirect } from "next/navigation";
+
 export const authClient = createAuthClient({
   /** The base URL of the server (optional if you're using the same domain) */
-  baseURL: "http://localhost:3000",
+  // baseURL: "http://localhost:3000",
 });
 
-export const signOut = () => {
-  authClient
-    .signOut()
-    .then(() => redirect("/"))
-    .catch((err) => console.error("ERROR: Error while signing out: ", err));
+export const signOut = async () => {
+  try {
+    await authClient.signOut();
+    window.location.href = "/";
+  } catch (err) {
+    console.error("ERROR: Error while signing out: ", err);
+  }
 };
--- next-app/proxy.ts
+++ next-app/proxy.ts
@@ -18,5 +18,5 @@ export async function proxy(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/chat"], // Specify the routes the middleware applies to
+  matcher: ["/chat/:path*"], // Specify the routes the middleware applies to
 };
