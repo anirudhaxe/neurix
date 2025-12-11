7625e9392f1192da7c51b84dce38b3195149318c
Aniruddha Chaudhary <axedotdev@outlook.com>
2025-12-11 21:04:21 +0530

    refactor: refactored chat component

--- next-app/app/(chat)/chat/page.tsx
+++ next-app/app/(chat)/chat/page.tsx
@@ -1,17 +1,6 @@
 import { redirect } from "next/navigation";
 import { generateId } from "ai";
-import auth from "@/lib/auth";
-import { headers } from "next/headers";
 
 export default async function Page() {
-  // access session in server component
-  const session = await auth.api.getSession({
-    headers: await headers(),
-  });
-
-  if (!session) {
-    redirect("/sign-in");
-  }
-
   redirect(`/chat/${generateId()}`);
 }
